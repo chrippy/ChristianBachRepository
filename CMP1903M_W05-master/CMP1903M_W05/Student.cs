aef2213c105e36604006b08adeddc2f214327dfa
Joseph Pitts <joepittsy@gmail.com>
2020-02-20 11:22:46 +0000

    first commit

---
+++ CMP1903M_W05-master/CMP1903M_W05/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMP1903M_W05
+{
+    class Student
+    {
+        //Some properties describing a Student
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int studentID { get; set; }
+        public string UniCollege { get; set; }
+        public string UniSchool { get; set; }
+        public int courseID { get; set; }
+        public string UniCourse { get; set; }
+    }
+}
